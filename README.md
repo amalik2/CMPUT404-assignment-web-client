fe7542c2bf5deba25c641d1931394df6bbf5564d
Adil Malik <malik2@cs.ualberta.ca>
2019-01-23 14:25:05 -0700

    finished requirements

--- README.md
+++ README.md
@@ -18,3 +18,50 @@ examples thus some of the code is Copyright © 2001-2013 Python
 Software Foundation; All Rights Reserved under the PSF license (GPL
 compatible) http://docs.python.org/2/library/socketserver.html
 
+Sources for Assignment 2
+========================
+
+Setting up a socket  
+[Question](https://stackoverflow.com/questions/7749341/basic-python-client-socket-example)  
+[Answer by David](https://stackoverflow.com/a/18297623)  
+Authors: [David](https://stackoverflow.com/users/521364/david), [Walrus the Cat](https://stackoverflow.com/users/1215687/walrus-the-cat), [dukbur](https://stackoverflow.com/users/992645/dukbur),
+[jww](https://stackoverflow.com/users/608639/jww)  
+License: [Creative Commons Attribute-ShareAlike 3.0](https://creativecommons.org/licenses/by-sa/3.0/)  
+
+Debugging socket errors  
+[Question](https://stackoverflow.com/questions/35969714/error-transport-endpoint-is-not-connected-python-sockets)  
+[Answer by Antti Haapala](https://stackoverflow.com/a/35970003)  
+Authors: [Antti Haapala](https://stackoverflow.com/users/918959/antti-haapala), [Atinesh](https://stackoverflow.com/users/653397/atinesh)  
+License: [Creative Commons Attribute-ShareAlike 3.0](https://creativecommons.org/licenses/by-sa/3.0/)  
+
+__str__ in python  
+[Question](https://stackoverflow.com/questions/4912852/how-do-i-change-the-string-representation-of-a-python-class)  
+[Answer by Mark Byers](https://stackoverflow.com/users/61974/mark-byers)  
+Authors: [Mark Byers](https://stackoverflow.com/users/61974/mark-byers), [snakile](https://stackoverflow.com/users/110028/snakile), [Elouan Keryell-Even](https://stackoverflow.com/users/3037171/elouan-keryell-even)  
+License: [Creative Commons Attribute-ShareAlike 3.0](https://creativecommons.org/licenses/by-sa/3.0/)  
+
+[Python URL Parse](https://docs.python.org/3/library/urllib.parse.html)  
+License: [Python Software Foundation License](https://docs.python.org/3/license.html#psf-license-agreement-for-python-release)  
+
+[urllib for encoding data as query strings](https://docs.python.org/3/library/urllib.parse.html)  
+License: [Python Software Foundation License](https://docs.python.org/3/license.html#psf-license-agreement-for-python-release)  
+
+[Socket documentation](https://docs.python.org/3/library/socket.html)  
+License: [Python Software Foundation License](https://docs.python.org/3/license.html#psf-license-agreement-for-python-release)  
+
+Python String Size in Bytes  
+[Question](https://stackoverflow.com/questions/30686701/python-get-size-of-string-in-bytes)  
+[Answer by Kris](https://stackoverflow.com/a/30686735)  
+Authors: [Iffat Fatima](https://stackoverflow.com/users/4406346/iffat-fatima), [Kris](https://stackoverflow.com/users/3783770/kris)  
+License: [Creative Commons Attribute-ShareAlike 3.0](https://creativecommons.org/licenses/by-sa/3.0/)  
+
+Websocket Handshake  
+https://developer.mozilla.org/en-US/docs/Web/API/WebSockets_API/Writing_WebSocket_servers  
+License: [Creative Commons Attribute-ShareAlike 3.0](https://creativecommons.org/licenses/by-sa/3.0/)  
+
+Structure of HTTP Requests in general  
+https://developer.mozilla.org/en-US/docs/Web/HTTP/Messages#HTTP_Requests  
+License: [Creative Commons Attribute-ShareAlike 3.0](https://creativecommons.org/licenses/by-sa/3.0/)  
+
+[SSL Socket](https://docs.python.org/3/library/ssl.html)  
+License: [Python Software Foundation License](https://docs.python.org/3/license.html#psf-license-agreement-for-python-release)  
--- httpclient.py
+++ httpclient.py
@@ -23,30 +23,75 @@ import socket
 import re
 # you may use urllib to encode data appropriately
 import urllib
+import ssl
+
+from urlparse import urlparse
 
 def help():
     print "httpclient.py [GET/POST] [URL]\n"
 
 class HTTPResponse(object):
-    def __init__(self, code=200, body=""):
+    def __init__(self, code=200, body="", headers="", statusMessage=""):
         self.code = code
         self.body = body
+        self.headers = headers
+        self.statusMessage = statusMessage
+
+    # Credit to Mark Byers at https://stackoverflow.com/a/4912856 for how to use __str__
+    def __str__(self):
+        return "HTTP/1.1 %d %s\r\n%s\r\n%s" % (self.code, self.statusMessage, self.headers, self.body)
+
+    def is_redirect(self):
+        return self.code == 301 or self.code == 302
+    
+    def get_header(self, header):
+        splitHeaders = self.headers.split("\r\n")
+        searchText = header + ":"
+        for headerDetails in splitHeaders:
+            if (searchText in headerDetails):
+                index = headerDetails.index(":") + 2
+                return headerDetails[index:]
+        return ""
 
 class HTTPClient(object):
     #def get_host_port(self,url):
 
     def connect(self, host, port):
-        # use sockets!
-        return None
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        if port == 443:
+            # code from: https://docs.python.org/3/library/ssl.html
+            context = ssl.create_default_context()
+            wrappedSocket = context.wrap_socket(sock, server_hostname=host)
+            wrappedSocket.connect((host, port))
+            return wrappedSocket
+        sock.connect((host, port))
+        return sock
 
     def get_code(self, data):
-        return None
+        return int(data[0].split(" ")[1])
 
     def get_headers(self,data):
-        return None
+        headers = ""
+        for line in data.split("\r\n"):
+            if (":" in line):
+                headers += line + "\r\n"
+            elif (line == ""):
+                break
+        return headers
 
-    def get_body(self, data):
-        return None
+    def get_body(self, lines):
+        body = ""
+        atBody = False
+        for line in lines:
+            if (line == "\r" and not atBody):
+                atBody = True
+            elif (atBody):
+                body += line + "\n"
+        return body
+
+    def get_status_message(self, data):
+        return " ".join(data[0].split(" ")[2:])
 
     # read everything from the socket
     def recvall(self, sock):
@@ -54,21 +99,89 @@ class HTTPClient(object):
         done = False
         while not done:
             part = sock.recv(1024)
+
             if (part):
                 buffer.extend(part)
+                tempBuf = str(buffer)
+                if ("301 Moved Permanently" in tempBuf and "Location: " in tempBuf and len(part) < 1024 and ("</body>" in tempBuf or "</html>" in tempBuf)):
+                    return tempBuf
             else:
                 done = not part
         return str(buffer)
 
+    def _get_socket_address(self, url):
+        details = urlparse(url)
+        port = details.port
+        host = details.netloc
+        if (port == None):
+            if (":" in host):
+                port = host.split(":")[1]
+            elif (details.scheme == "https"):
+                port = 443
+            else:
+                port = 80
+        if (":" in host):
+            host = host.split(":")[0]
+        return (host, port, details.path)
+
+    def _parse_response(self, response):
+        headers = ""
+        statusMessage = ""
+
+        lines = response.split("\n")
+        # TODO: remove this if not needed
+        if (len(lines) == 1 and lines[0] == ""):
+            print("EMPTY RESPONSE")
+            body = ""
+            code = 400
+        else:
+            headers = self.get_headers(response)
+            body = self.get_body(lines)
+            code = self.get_code(lines)
+            statusMessage = self.get_status_message(lines)
+        return HTTPResponse(code, body, headers, statusMessage)
+
+    def _send_request(self, url, method, requestConcat):
+        address = self._get_socket_address(url)
+        sock = self.connect(address[0], address[1])
+        host = address[0] + ":" + str(address[1])
+        path = address[2]
+        if (path == ""):
+            path = "/"
+
+        if (method == "GET"):
+            sock.sendall("GET %s HTTP/1.1\r\nHost: %s\r\nConnection: Upgrade\r\nUpgrade: websocket\r\n\r\n" % (path, host))
+        #print("%s %s HTTP/1.1\r\nHost: %s\r\nAccept: */*\r\n%s" % (method, path, host, requestConcat))
+        sock.sendall("%s %s HTTP/1.1\r\nHost: %s\r\nAccept: */*\r\n%s" % (method, path, host, requestConcat))
+
+        response = self.recvall(sock)
+        sock.close()
+        responseData = self._parse_response(response)
+        # TODO: remove the below if it is not needed
+        """ if (responseData.is_redirect()):
+            redirectURL = responseData.get_header("Location")
+            return self._send_request(redirectURL, method, requestConcat) """
+        return responseData
+
+    def _parse_args(self, args):
+        if (args == None):
+            return ""
+        return urllib.urlencode(args)
+
+    def _get_byte_length(self, string):
+        # Credit to Kris at https://stackoverflow.com/a/30686735 for string byte length
+        return len(string.encode("utf-8"))
+
     def GET(self, url, args=None):
-        code = 500
-        body = ""
-        return HTTPResponse(code, body)
+        return self._send_request(url, "GET", "\r\n")
 
     def POST(self, url, args=None):
-        code = 500
-        body = ""
-        return HTTPResponse(code, body)
+        # TODO: is this needed?
+        #concat = "Accept: application/x-www-form-urlencoded, application/json, text/plain, "
+        data = self._parse_args(args)
+        concat = "Content-Type: application/x-www-form-urlencoded\r\nContent-Length: %d\r\n\r\n%s" % (self._get_byte_length(data), data)
+        response = self._send_request(url, "POST", concat)
+        return response
 
     def command(self, url, command="GET", args=None):
         if (command == "POST"):
